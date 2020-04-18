bc2c2e72d8cfe643674439e50c71859632c75952
StanislavKaisin <8050361@gmail.com>
2020-04-19 00:22:57 +0300

    recover from babel crash

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+// import Cards from './components/Cards/Cards'
+// import Chart from './components/Chart/Chart'
+// import CountryPicker from './components/CountryPicker/CountryPicker'
+
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from './App.module.css'
+import { fetchData } from './api/index.js'
+
+class App extends Component {
+  state={
+    data:{}
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData})
+  }
+  
+  render() {
+    const {data}=this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryPicker = () => {
+  return (
+    <div>
+      <h2>CountryPicker</h2>
+    </div>
+  )
+}
+
+export default CountryPicker;
